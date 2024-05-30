274f827e962c7fdda3f1bd55aed4e4b1c41e28c3
Hassen <h.hassen.malek@gmail.com>
2024-05-30 10:57:45 +0200

    recupérattion des données

--- resoc_n1/news.php
+++ resoc_n1/news.php
@@ -65,7 +65,7 @@
                     posts.created,
                     users.alias as author_name,  
                     count(likes.id) as like_number,  
-                    GROUP_CONCAT(DISTINCT tags.label) AS taglist 
+                    GROUP_CONCAT(DISTINCT tags.label) as taglist 
                     FROM posts
                     JOIN users ON  users.id=posts.user_id
                     LEFT JOIN posts_tags ON posts.id = posts_tags.post_id  
